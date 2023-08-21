cb8a7feecb0d000fd380685f58726a74740ea6e3
ulisesantana <ulisesantana@users.noreply.github.com>
2023-08-21 14:04:12 +0000

    deploy: 35265219bcf96684c1fa8e96e7ad4c8267504f09

---
+++ components/index.js
@@ -0,0 +1,7 @@
+// Once build is done all JS components are created in the same folder
+import './animation-portfolio-item.js'
+import './img-caption.js'
+import './theme-switch.js'
+import './tldr-section.js'
+import './scroll-to-top.js'
+import './youtube-video.js'
