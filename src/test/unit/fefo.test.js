35265219bcf96684c1fa8e96e7ad4c8267504f09
Ulises Santana <ulisesantana@gmail.com>
2023-08-21 14:23:54 +0100

    ✅ remove unecessary test and derivates

--- src/test/unit/fefo.test.js
+++ src/test/unit/fefo.test.js
@@ -1,7 +1,6 @@
 const { strict: assert } = require('node:assert')
 const { describe, it } = require('node:test')
 const fefo = require('../../data/fefo')
-const { generatePost } = require('../builders/post')
 
 describe('fefo should', () => {
   describe('work whit dates', () => {
@@ -15,43 +14,6 @@ describe('fefo should', () => {
       assert.equal(
         fefo.dates.toLocaleDateString(date, 'en'),
         'Wednesday, March 1, 2023'
-      )
-    })
-  })
-
-  describe('work with blog posts', () => {
-    const posts = [
-      generatePost({
-        url: '/test1',
-        tags: ['software', 'food']
-      }),
-      generatePost({
-        url: '/test2',
-        tags: ['software']
-      }),
-      generatePost({
-        url: '/test3',
-        tags: ['food']
-      })
-    ]
-
-    describe('returning the related posts', () => {
-      it('successfully', () => {
-        assert.deepEqual(
-          fefo.blog.getRelatedPosts(posts[2].page.url, posts),
-          [posts[0]]
-        )
-        assert.deepEqual(
-          fefo.blog.getRelatedPosts(posts[0].page.url, posts),
-          [posts[1], posts[2]]
-        )
-      })
-    })
-
-    it('returning tags from post by url', () => {
-      assert.deepEqual(
-        fefo.blog.getTags(posts[0].page.url, posts),
-        posts[0].data.tags
       )
     })
   })
