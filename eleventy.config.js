bbbf1c08ff6132e798f0d47c0e4da6a8db9073f2
Ulises Santana <ulisesantana@gmail.com>
2023-08-21 15:16:45 +0100

    ♻️ move config from package.json to eleventy config file

--- eleventy.config.js
+++ eleventy.config.js
@@ -18,6 +18,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addDataExtension('yaml', contents => yaml.load(contents))
 
   return {
+    pathPrefix: '/ago-web/',
     dir: {
       input: 'src/pages',
       includes: '../components',
